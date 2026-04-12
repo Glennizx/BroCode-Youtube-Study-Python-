03e24ec7d63a5158c88d9b199ebfcf7fc04d575b
Gelade <glennezekielmiral@gmail.com>
2026-04-12 01:40:49 +0000

    commit

---
+++ 2. Type casting.py
@@ -0,0 +1,17 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+# print(type(age)) --> Use this in order to know the data type 
+
+name = "Bro Code"
+age = 25
+gpa = 3.2
+is_student = True
+
+name = bool(name)
+print(name)
+
+#age is currently an int data type
+
+age = float(age)
+#Now we type cast it into float. From int to float
+print(age)
