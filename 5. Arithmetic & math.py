3a7cdf4bfc03bd9d1dd065d07ab397e902ac403b
Gelade <glennezekielmiral@gmail.com>
2026-04-14 06:03:42 +0000

    commit

---
+++ 5. Arithmetic & math.py
@@ -0,0 +1,18 @@
+
+
+#friends = 10
+
+#friends = friends + 1
+#friends += 1  --> Augmented assignment operator
+#friends -= 2
+#friends *=  3
+#friends /= 2
+#friends = friends ** 2  --> friends to the power of 2 
+#friends **= 2
+#remainder = friends % 3  --> will give a result of a remainder. e.g. 10/3 = 3, Output will be 1
+
+
+
+x = 3.14
+y = 4
+z = 5
