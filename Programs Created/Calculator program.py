85481b42318d218f844eb3ee0da6c4d142bf2d58
Gelade <glennezekielmiral@gmail.com>
2026-04-14 06:55:00 +0000

    commit

---
+++ Programs Created/Calculator program.py
@@ -0,0 +1,7 @@
+# Python calculator
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+print(num1 + num2)
