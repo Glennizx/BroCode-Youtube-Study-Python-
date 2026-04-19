6e2cce7b67e8e55dca32c370580fc036ea9c3d4a
Gelade <glennezekielmiral@gmail.com>
2026-04-19 07:41:28 +0000

    commit

--- 7. Calculator program.py
+++ 7. Calculator program.py
@@ -4,4 +4,20 @@ operator = input("Enter an operator (+ - * /): ")
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
-print(num1 + num2)
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 2))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 2))
+
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 2))
+
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 2))
+          
+else:
+    print(f"{operator} is not valid operator")
