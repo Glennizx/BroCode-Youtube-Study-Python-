2dff0dc7fc26e290f882467e7d4f484ad10d9acb
Gelade <glennezekielmiral@gmail.com>
2026-05-07 12:05:21 +0000

    commit

---
+++ 13. String indexing.py
@@ -0,0 +1,9 @@
+# Indexing = Accesing elements of a sequence using [] (Indexing operator)
+#             [start : end :  step]
+
+credit_number = '1234-5678-9012-3456'
+
+credit_number = credit_number[::-1] # Reverses the string
+
+print((credit_number))   # 6543-2109-8765-4321@
+
