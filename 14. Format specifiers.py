0efcb23a172e5763b5a31d7503ff1ee000bbeeaf
Gelade <glennezekielmiral@gmail.com>
2026-05-07 13:16:32 +0000

    commit

---
+++ 14. Format specifiers.py
@@ -0,0 +1,23 @@
+# Format specifiers = {value:flags} format a value based on what
+#                       flags are inserted
+
+price1 = 30000.14159
+price2 = -9870.65
+price3 = 1200.34
+
+print(f"Price 1 is ${price1:+,.2f}") #f means floating point number
+print(f"Price 2 is ${price2:+,.2f}")
+print(f"Price 3 is ${price3:+,.2f}")
+
+
+#Notes
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocate that many spaces
+# :03 = allocate and zero pad that many spaces
+# :< = left justify
+# :> right justify
+# :^ = center align
+# :+ = use plus sign to indicate positive value
+# := = place sign to leftmost position
+# : =insert a space before positive numbers
+# :, comma separator
