7f135fc4e5b03e5a8120b092fcfaacb2bc958320
Gelade <glennezekielmiral@gmail.com>
2026-05-07 13:33:36 +0000

    commit

--- 15. While loops..py
+++ 15. While loops..py
@@ -1,1 +1,11 @@
 # while loop = execuse some code WHILE some condition remains true
+
+
+num = int(input("Enter a number between 1 - 10: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a # between 1 - 10: "))
+
+
+print(f"Your number is {num}")
