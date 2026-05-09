5d3c82e1f0e9e68543e9cda38cd316dc76cea82c
Gelade <glennezekielmiral@gmail.com>
2026-05-09 05:40:06 +0000

    commit

---
+++ 17. For loops.py
@@ -0,0 +1,9 @@
+# for loops = execute a block of code a fixed number  of times.
+#            You can iterate over a range, string, sequence, etc.
+
+
+for x in range(1,21):
+    if  x == 13: #if x is 13, skip the rest of the code and continue to the next iteration
+        continue
+    else:
+        print(x) 
